d52006d53f06ab8d916e55230430fc73a257bba5
Awais Abdul Khaliq <awais.a.khaliq@gmail.com>
2015-07-22 21:08:55 +0500

    Login System
    bcrypt-nodejs, coupled with the passport library... has callback throw
    error on login and failure to start after running once

--- config/routes.js
+++ config/routes.js
@@ -34,8 +34,19 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
 
+  'get /login': {
+       view: 'login'
+  },
+
+  'post /login': 'AuthController.login',
+
+  '/logout': 'AuthController.logout',
+
+  'get /signup': {
+    view: 'signup'
+  }
   /***************************************************************************
   *                                                                          *
   * Custom routes here...                                                    *
