658fa6409fb42c5ea22cff393512c572199110d2
ahsangondal <ahsangondal15@gmail.com>
2015-07-27 14:07:32 +0500

    f

--- config/routes.js
+++ config/routes.js
@@ -33,8 +33,8 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': {
-    controller: 'user',
-    view:'index'
+    controller: 'user'
+    
   },
 
 
