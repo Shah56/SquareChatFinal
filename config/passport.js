8adea22c69e227fadcaf15cc4334d64e7db50756
ahsangondal <ahsangondal15@gmail.com>
2015-07-27 12:11:41 +0500

    fbk shit

--- config/passport.js
+++ config/passport.js
@@ -78,33 +78,4 @@ passport.use(new FacebookStrategy({
 
 
 
-passport.use(new LocalStrategy({
-    usernameField: 'email',
-    passwordField: 'password'
-  },
-  function(email, password, done) {
-
-    User.findOne({ email: email }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect email.' });
-      }
-
-      bcrypt.compare(password, user.password, function (err, res) {
-          if (!res)
-            return done(null, false, {
-              message: 'Invalid Password'
-            });
-          var returnUser = {
-            email: user.email,
-            createdAt: user.createdAt,
-            id: user.id
-          };
-          return done(null, returnUser, {
-            message: 'Logged In Successfully'
-          });
-        });
-    });
-  }
-));
 
